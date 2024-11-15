5854146bfd1b0682a372971844bf2c675134da71
Luis Antonio León Pedroza <up210289@alumnos.upa.edu.mx>
2024-11-14 21:15:36 -0600

    Drawer creation

--- agronova-app/app/(tabs)/_layout.tsx
+++ agronova-app/app/(tabs)/_layout.tsx
@@ -1,65 +1,75 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity, Image, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import SideMenu from '@/components/widgets/SideMenu'; 
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [drawerVisible, setDrawerVisible] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerVisible(!drawerVisible);
+  };
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#4d7c0f', 
-        tabBarInactiveTintColor: '#65a30d',
-        headerStyle: {
-          backgroundColor: '#f0f9ff',
-        },
-        headerShadowVisible: true,
-        headerTintColor: '#cae9ff',
-        tabBarStyle: {
-          backgroundColor: '#7dd3fc',
-        },
-        headerLeft: () => (
-          <Image
-            source={require('@/assets/images/logo_text.png')} 
-            style={{ width: 200, height: 50, marginLeft: 10 }}
-          />
-        ),
-        headerRight: () => (
-          <TouchableOpacity onPress={() => alert('Botón presionado!')}>
-            <Ionicons
-              name="menu" 
-              size={24}
-              color={Colors[colorScheme ?? 'light'].text}
-              style={{ marginRight: 10 }}
+    <View style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#4d7c0f', 
+          tabBarInactiveTintColor: '#65a30d',
+          headerStyle: {
+            backgroundColor: '#f0f9ff',
+          },
+          headerShadowVisible: true,
+          headerTintColor: '#cae9ff',
+          tabBarStyle: {
+            backgroundColor: '#7dd3fc',
+          },
+          headerLeft: () => (
+            <Image
+              source={require('@/assets/images/logo_text.png')} 
+              style={{ width: 200, height: 50, marginLeft: 10 }}
             />
-          </TouchableOpacity>
-        ),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: '',
-          tabBarIcon: ({ color, focused }) => (
-            <FontAwesome6 name={focused? 'plant-wilt': 'plant-wilt'} color={color}  size={24} />
+          ),
+          headerRight: () => (
+            <TouchableOpacity onPress={toggleDrawer}>
+              <Ionicons
+                name="menu" 
+                size={24}
+                color={Colors[colorScheme ?? 'light'].text}
+                style={{ marginRight: 10 }}
+              />
+            </TouchableOpacity>
           ),
         }}
-      />
-      <Tabs.Screen
-        name="organization"
-        options={{
-          title: '',
-          tabBarIcon: ({ color, focused }) => (
-            <MaterialCommunityIcons name={focused? 'hoop-house': 'hoop-house'} color={color} size={30} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: '',
+            tabBarIcon: ({ color, focused }) => (
+              <FontAwesome6 name={focused ? 'plant-wilt' : 'plant-wilt'} color={color} size={24} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="organization"
+          options={{
+            title: '',
+            tabBarIcon: ({ color, focused }) => (
+              <MaterialCommunityIcons name={focused ? 'hoop-house' : 'hoop-house'} color={color} size={30} />
+            ),
+          }}
+        />
+      </Tabs>
+
+      {/* Drawer personalizado */}
+      <SideMenu visible={drawerVisible} onClose={() => setDrawerVisible(false)} />
+    </View>
   );
 }
---
+++ agronova-app/components/widgets/SideMenu.tsx
@@ -0,0 +1,156 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, Animated, Text, TouchableOpacity, Image } from 'react-native';
+import { Drawer } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+interface SideMenuProps {
+  visible: boolean;
+  onClose: () => void;
+}
+
+const SideMenu: React.FC<SideMenuProps> = ({ visible, onClose }) => {
+  const slideAnim = useRef(new Animated.Value(300)).current;
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (visible) {
+      Animated.timing(slideAnim, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(slideAnim, {
+        toValue: 300,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [visible]);
+
+  const handleItemPress = (item: string) => {
+    setSelectedOption(item); // Actualiza el estado con la opción seleccionada
+    onClose(); // Cierra el menú cuando se selecciona una opción
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      transparent={true}
+      animationType="none"
+      onRequestClose={onClose}
+    >
+      <TouchableWithoutFeedback onPress={onClose}>
+        <View style={styles.modalOverlay}>
+          <TouchableWithoutFeedback>
+            <Animated.View style={[styles.drawerContainer, { transform: [{ translateX: slideAnim }] }]}>
+              <LinearGradient
+                colors={['#f0f9ff', '#e0f2fe', '#bae6fd', '#7dd3fc']}
+                style={styles.gradientBackground}
+              >
+                <Drawer.Section>
+                  <TouchableOpacity
+                    onPress={() => handleItemPress('usuario')} // Pasa la opción "usuario"
+                    style={[
+                      styles.drawerItem,
+                      selectedOption === 'usuario' && styles.selectedItem, // Cambia el estilo si es la opción seleccionada
+                    ]}
+                  >
+                    <Image
+                      source={{
+                        uri: 'https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg',
+                      }}
+                      style={styles.icon}
+                    />
+                    <Text style={styles.itemText}>Usuario</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => handleItemPress('ayuda')} // Pasa la opción "ayuda"
+                    style={[
+                      styles.drawerItem,
+                      selectedOption === 'ayuda' && styles.selectedItem, // Cambia el estilo si es la opción seleccionada
+                    ]}
+                  >
+                    <MaterialIcons name="settings" size={24} color="#65a30d" />
+                    <Text style={styles.itemText}>Ayuda y Soporte</Text>
+                  </TouchableOpacity>
+                </Drawer.Section>
+
+                <View style={styles.spacer} />
+
+                <TouchableOpacity style={styles.logoutButton} onPress={onClose}>
+                  <Text style={styles.logoutButtonText}>Log out</Text>
+                </TouchableOpacity>
+              </LinearGradient>
+            </Animated.View>
+          </TouchableWithoutFeedback>
+        </View>
+      </TouchableWithoutFeedback>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  drawerContainer: {
+    width: '70%',
+    height: '100%',
+    borderTopLeftRadius: 30,  // Mayor radio de las esquinas
+    borderBottomLeftRadius: 30,  // Mayor radio de las esquinas
+    overflow: 'hidden',
+  },
+  gradientBackground: {
+    flex: 1,
+    padding: 20,
+  },
+  itemText: {
+    fontSize: 16,
+    color: '#0c4a6e',
+    paddingVertical: 10,
+    paddingLeft: 10,
+    fontWeight: 'bold',
+  },
+  icon: {
+    width: 30,  // Tamaño de la imagen
+    height: 30,
+    borderRadius: 15,  // Hacemos la imagen circular
+    marginRight: 15,  // Mayor separación entre la imagen y el texto
+    alignSelf: 'center', // Centra la imagen verticalmente
+  },
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center', // Alineamos verticalmente el texto con la imagen
+    justifyContent: 'flex-start', // Aseguramos que el contenido esté alineado a la izquierda
+    paddingVertical: 15,  // Añadimos más espacio arriba y abajo
+    borderRadius: 10,  // Añadimos borde redondeado a los ítems
+    paddingHorizontal:10
+  },
+  selectedItem: {
+    backgroundColor: '#dbeafe', // Color de fondo cuando está seleccionado
+    borderRadius: 20, // Redondeamos las esquinas del item
+  },
+  spacer: {
+    flexGrow: 1,
+  },
+  logoutButton: {
+    backgroundColor: '#0369a1',
+    paddingVertical: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  logoutButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default SideMenu;
