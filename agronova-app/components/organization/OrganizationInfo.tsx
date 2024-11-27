30afaacba13779a352f5d1e0b624a79e7bc93072
Luis Antonio León Pedroza <up210289@alumnos.upa.edu.mx>
2024-11-27 15:22:13 -0600

    Implementation of the "Delete Ranch" button view

--- agronova-app/components/organization/OrganizationInfo.tsx
+++ agronova-app/components/organization/OrganizationInfo.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert, Text, TouchableOpacity } from 'react-native';
 import { ThemedText } from '@/components/widgets/ThemedText';
 import { ThemedView } from '@/components/widgets/ThemedView';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -6,19 +6,36 @@ import MembersList from './membersList'; // Asegúrate de importar el componente
 import React from 'react';
 
 export default function Organization() {
+  const handleDeletePress = () => {
+    Alert.alert(
+      'Eliminar rancho',
+      '¿Estás seguro de que deseas eliminar el rancho?',
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        { text: 'Eliminar', style: 'destructive', onPress: () => console.log('Rancho eliminado') }
+      ]
+    );
+  };
+
   return (
     <LinearGradient
       colors={['#f0f9ff', '#e0f2fe', '#bae6fd', '#7dd3fc']}
       style={{ flex: 1 }}
     >
       <ThemedView style={{ flex: 1, padding: 16 }} lightColor="transparent" darkColor="transparent">
-        <View>
+        <View style={{ flex: 1 }}>
           <ThemedText style={styles.titleText}>Rancho "Las Camelinas"</ThemedText>
           <ThemedText style={styles.descriptionText}>
             Empresa líder en producción de maíz a nivel nacional, con más de 30 años de experiencia en el sector.
           </ThemedText>
           {/* Aquí llamas al componente MembersList */}
           <MembersList />
+        </View>
+        {/* Botón para eliminar rancho */}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.deleteButton} onPress={handleDeletePress}>
+            <Text style={styles.buttonText}>Eliminar rancho</Text>
+          </TouchableOpacity>
         </View>
       </ThemedView>
     </LinearGradient>
@@ -36,5 +53,23 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginVertical: 10,
     color: '#0c4a6e',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 20, // Añadir padding para separar el botón del borde inferior
+  },
+  deleteButton: {
+    backgroundColor: '#ff0000',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 25, // Ajustar este valor para hacer el botón más ovalado
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
