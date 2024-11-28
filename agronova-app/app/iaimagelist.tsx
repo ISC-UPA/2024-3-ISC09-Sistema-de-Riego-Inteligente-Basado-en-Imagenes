68bece49d85fd1f90b28eecf7431d4d4373af834
Sofia Calderon <up210300@alumnos.upa.edu.mx>
2024-11-28 03:28:41 -0600

    Added function to see images screen

--- agronova-app/app/iaimagelist.tsx
+++
@@ -1,8 +0,0 @@
-import IAimagesList from "@/components/iaimages/iaimage";
-import React from "react";
-
-export default function LoginScreen() {
-    return(
-        <IAimagesList></IAimagesList>
-    )
-}
--- agronova-app/components/iaimages/iaimage.tsx
+++ agronova-app/components/iaimages/iaimage.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, ScrollView, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Stack } from 'expo-router';
 import IAimageCard from '@/components/iaimages/ImageCard';
+import { IconButton } from 'react-native-paper';
+import { CropContext } from '../context/CropContext';
 
 export default function IAimagesList() {
+  const cropContext = useContext(CropContext);
+
+  if (!cropContext) {
+    throw new Error('CropContext debe estar dentro del proveedor CropProvider');
+  }
+
+  const { setImages } = cropContext;
+
+  const handleBackPress = () => {
+    setImages(false);
+  };
+
   return (
     <LinearGradient
       colors={['#f0f9ff', '#e0f2fe', '#bae6fd', '#7dd3fc']}
       style={{ flex: 1 }}
     >
-      <Stack.Screen options={{ title: '', headerShown: false }} />
+      <View style={styles.headerContainer}>
+        <IconButton icon="chevron-left" size={24} onPress={handleBackPress} />
+        <Text style={styles.titleText}>Historial de imágenes</Text>
+      </View>
       <View style={styles.container}>
-        <Text style={styles.titleText}>Rancho "Nombre del Rancho"</Text>
-        <Text style={styles.subtitleText}>Historial de imágenes</Text> {/* Subtítulo agregado */}
         <ScrollView style={styles.scrollView}>
           {/* Aquí llamamos al componente IAimageCard */}
           <IAimageCard />
@@ -29,6 +44,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   titleText: {
     fontSize: 24,
